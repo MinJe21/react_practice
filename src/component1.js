765fe1a3631ae3d016e4087ddde5a6c9a3bb6afa
김민제 <minje9370@handong.ac.kr>
2024-11-07 16:08:08 +0900

    react hw4-2

--- src/component1.js
+++ src/component1.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Component1 extends Component {
   render() {
     return (
-      <div>Hello World!!</div>
+      <div>This class is Open Source Studio in HGU!!</div>
     )
   }
 }
